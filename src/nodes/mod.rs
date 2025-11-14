655d5214096b56bd1ea1d3d8a7cbe6938da5e5cc
Rafael Milewski` <rafael.milewski@gmail.com>
2025-11-14 22:02:41 +0800

    add rename file node

--- src/nodes/mod.rs
+++ src/nodes/mod.rs
@@ -4,3 +4,4 @@ mod json_loader;
 mod json_path_extractor;
 mod load_image_from_path;
 mod collapse_image_list;
+mod rename_file;
---
+++ src/nodes/rename_file.rs
@@ -0,0 +1,22 @@
+use comfy_builder_core::prelude::*;
+use std::error::Error;
+use std::fs;
+
+#[derive(NodeInput)]
+struct Input {
+    from: String,
+    to: String,
+}
+
+#[node(category = "Rusty Nodes / Utilities", description = "Rename a file.")]
+struct RenameFile;
+
+impl Node for RenameFile {
+    type In = Input;
+    type Out = ();
+    type Error = Box<dyn Error + Send + Sync>;
+
+    fn execute(&self, input: Self::In) -> Result<Self::Out, Self::Error> {
+        Ok(fs::rename(input.from, input.to)?)
+    }
+}
